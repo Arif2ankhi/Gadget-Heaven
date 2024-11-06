f6b9b096015a6a2d33e859b25ed51abadd23ac53
Arif2ankhi <arif2ankhi@gmail.com>
2024-11-06 05:09:28 +0000

    Cart Added

---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { AiFillDelete } from "react-icons/ai";
+
+const Cart = () => {
+    return (
+        <>
+         <div className="relative bg-base-200 my-8 rounded-xl p-6">
+            <div className="absolute top-4 right-4 text-rose-600 text-3xl cursor-pointer">
+            <AiFillDelete />
+            </div>
+
+            <div className="flex gap-3">
+                <img
+                    src={image}
+                    alt="Product"
+                    className="w-52 h-36 rounded-lg shadow-2xl"
+                />
+                <div>
+                    <h1 className="text-xl font-bold">{title}</h1>
+                    <p className="py-6">{description}</p>
+                    <p>Price: $ {price}</p>
+                </div>
+            </div>
+        </div>
+
+        </>
+    );
+};
+
+export default Cart;
+
